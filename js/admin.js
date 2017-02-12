c528ca79ee98583d14a70f8f84e2b36a3ecb6cc7
Joel Andy <joel@waqastudios.com>
2017-02-12 11:44:55 -0500

    finanza

--- js/admin.js
+++ js/admin.js
@@ -53,8 +53,7 @@ $scope.segundofiltro=true
     console.log('finanzas....',$scope.listafinance)
     });
 
-  $http.get(host+"/listafinance/").success(function(response) {$scope.listafinance = response;
-    });
+
 
   $http.get(host+"/listparametros/").success(function(response) {$scope.listparametros = response;
 
